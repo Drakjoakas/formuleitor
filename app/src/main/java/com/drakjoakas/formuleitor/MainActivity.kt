fdaa6295ab0ec40c52b60ed1c82afdb6aa027d7b
JOAQUIN SANDOVAL MIRAMONTES <joakas.11@gmail.com>
2022-04-28 22:41:48 -0500

    Se agregaron recursos y el spinner.

--- app/src/main/java/com/drakjoakas/formuleitor/MainActivity.kt
+++ app/src/main/java/com/drakjoakas/formuleitor/MainActivity.kt
@@ -2,10 +2,35 @@ package com.drakjoakas.formuleitor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import com.drakjoakas.formuleitor.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.spinnerFormula.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long) {
+                TODO("Not yet implemented")
+                print(parent?.getItemAtPosition(position)?.toString())
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
     }
+
+
 }
